0bf47081c48dfe64c470925ab56533d7607078b0
ayush0711 <ayush.mittal_cs17@gla.ac.in>
2019-11-14 16:19:58 +0530

    read line by line and print list

--- file.py
+++ file.py
@@ -1,7 +1,5 @@
-#read last n lines of a file
-
+#read a file line by line and store it on a list
 f=open('C:/Users/hp/PycharmProjects/untitled2/calc.txt','r')
 data=f.readlines()
-print(data[-1])
+print(data)
 f.close()
-
