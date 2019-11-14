d8f5f3c9b8f42a6396bf65b520ea05847650a33e
ayush0711 <ayush.mittal_cs17@gla.ac.in>
2019-11-14 16:01:33 +0530

    append text and display

--- file.py
+++ file.py
@@ -1,16 +1,10 @@
-# write a python program to read  an entire text file
+#append text to a file and display the text
 
+f=open('C:/Users/hp/PycharmProjects/untitled2/calc.txt','a+')
+f.seek(0)
+f.write('\nkanha')
+f.close()
 f=open('C:/Users/hp/PycharmProjects/untitled2/calc.txt','r')
 data=f.read()
 print(data)
-f.close()
-
-
-#read n no. of lines
-
-f=open('C:/Users/hp/PycharmProjects/untitled2/calc.txt','r')
-a=int(input())
-for i in range(a):
-    a=f.readline()
-    print(a)
 f.close()
